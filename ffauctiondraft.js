c6611a5fef2e3a55b42ff6cbfa0d7ba56ac94887
Kyle M <maestasks@gmail.com>
2015-02-11 22:46:14 -0800

    first commit

---
+++ ffauctiondraft.js
@@ -0,0 +1,12 @@
+//player constructor
+function Player(name, position, price) {
+	this.playerName = name;
+	this.position = position;
+	this.price = price;
+};
+
+var aaronRodgers = new Player ("Aaron Rodgers", "QB", 50);
+var leveonBell = new Player ("LeVeon Bell", "RB", 55);
+var julioJones = new Player ("Julio Jones", "WR", 40);
+var robGronkowski = new Player ("Rob Gronkowski", "TE", 52);
+
